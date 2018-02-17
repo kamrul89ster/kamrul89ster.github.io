adae75852038d5dbe1f7970575213c9a869f01b8
basselhajjo <basselhajjo@gmail.com>
2018-02-17 17:43:17 +0100

    new nav bar

--- code/javascript.js
+++ code/javascript.js
@@ -1,3 +1,5 @@
+//slide show
+
 const slides = document.querySelectorAll('#slides .sliderImg');
 let currentSlide = 0;
 let slideInterval = setInterval(nextSlide,2000);
@@ -7,3 +9,22 @@ function nextSlide(){
     currentSlide = (currentSlide+1)%slides.length;
     slides[currentSlide].className = 'sliderImg showing';
 }
+
+//menu toggle-button
+
+$(document).ready(function(){
+    $(".menu-icon").on("click",function(){
+        $("nav ul").toggleClass("navShowing")
+    })
+})
+
+$(window).on("scroll",function(){
+    if($(window).scrollTop()){
+        $('nav').addClass('black');
+    }else{
+        $('nav').removeClass('black');
+    }
+})
+
+//scrolling effect
+
