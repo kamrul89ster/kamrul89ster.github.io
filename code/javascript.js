6a3e2a8b937026ae6fa62ad69dedfa409a37e05e
basselhajjo <basselhajjo@gmail.com>
2018-03-05 17:52:41 +0100

    messages system works

--- code/javascript.js
+++ code/javascript.js
@@ -19,14 +19,14 @@ $(document).ready(function(){
 })
 
 //
-//$(window).on("scroll",function(){
-//    if($(window).scrollTop()){
-//        $('nav').addClass('black');
-//    }else{
-//        $('nav').removeClass('black');
-//    }
-//})
-//
+$(window).on("scroll",function(){
+    if($(window).scrollTop()){
+        $('nav').addClass('black');
+    }else{
+        $('nav').removeClass('black');
+    }
+})
+
 //scrolling effect
 
 //
